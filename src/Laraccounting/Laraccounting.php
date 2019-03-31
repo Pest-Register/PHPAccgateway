f359ddc06274b32680d5b047cd0f471eabe092c1
Dylan <dylan@pestregister.com>
2019-04-01 10:13:47 +1100

    First Push

---
+++ src/Laraccounting/Laraccounting.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dylan
+ * Date: 1/04/2019
+ * Time: 9:59 AM
+ */
+
+namespace Laraccounting;
+
+class Laraccounting
+{
+    public static function world() {
+        return 'Hello World';
+    }
+}
